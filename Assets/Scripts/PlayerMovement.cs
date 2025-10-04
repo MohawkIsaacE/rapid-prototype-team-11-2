f1ffa11c6f2835a4894290fa1b65cc9f5e29c8df
Isaac <isaac.ebert@mohawkcollege.ca>
2025-10-04 15:07:06 -0400

    Add restart

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static UnityEngine.GraphicsBuffer;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,47 +11,60 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     private float moveSpeed = 3;
     public Camera cam;
+    private bool isAlive;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isAlive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Movement
-        float moveInputX = Input.GetAxis("Horizontal");
-        float moveInputY = Input.GetAxis("Vertical");
-
-        rb.velocity = new Vector2(moveInputX * moveSpeed, moveInputY * moveSpeed);
-
-        // Player teleport/dash in the direction they are looking
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isAlive)
         {
-            rb.transform.position += transform.up * 5f;
+            // Movement
+            float moveInputX = Input.GetAxis("Horizontal");
+            float moveInputY = Input.GetAxis("Vertical");
+
+            rb.velocity = new Vector2(moveInputX * moveSpeed, moveInputY * moveSpeed);
+
+            // Player teleport/dash in the direction they are looking
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.transform.position += transform.up * 5f;
+            }
+
+            // Camera follow player
+            cam.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y, cam.transform.position.z);
+
+            // Look at mouse (copied from an answer at https://discussions.unity.com/t/2d-look-at-mouse-position-z-rotation-c/117860/2)
+            // convert mouse position into world coordinates
+            Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // get direction you want to point at
+            Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+            // set vector of transform directly
+            transform.up = direction;
         }
-        
-        // Camera follow player
-        cam.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y, cam.transform.position.z);
-
-        // Look at mouse (copied from an answer at https://discussions.unity.com/t/2d-look-at-mouse-position-z-rotation-c/117860/2)
-        // convert mouse position into world coordinates
-        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // get direction you want to point at
-        Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
-
-        // set vector of transform directly
-        transform.up = direction;
+        else
+        {
+            // Reset game
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Projectile")
+        if (collision.gameObject.tag == "Projectile" || collision.gameObject.tag == "Beam")
         {
-            Destroy(collision.gameObject);
+            gameObject.GetComponent<Renderer>().enabled = false;
+            isAlive = false;
         }
     }
 }
